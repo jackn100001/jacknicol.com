94442acb02f517f10654f46c49fe902f7d1a98ed
Jack <jack-nicol1@hotmail.co.uk>
2021-06-26 21:22:25 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,44 @@
+// index.js
+
+/**
+ * Required External Modules
+ */
+const express = require("express");
+const path = require("path");
+
+/**
+ * App Variables
+ */
+const app = express();
+const port = process.env.PORT || "8000";
+
+/**
+ *  App Configuration
+ */
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(__dirname + '/public'));
+
+/**
+ * Routes Definitions
+ */
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "views/") + 'index.html');
+});
+
+app.get("/text-file", (req, res) => {
+    var filename = req.query.filename + ".txt";
+    var fs = require('fs');
+    fs.readFile("public/" + filename, 'utf8', function(err, data) {
+        if (err) throw err;
+        console.log('OK: ' + filename);
+        console.log(data)
+        res.send(data);
+    });
+});
+
+/**
+ * Server Activation
+ */
+ app.listen(port, () => {
+    console.log(`Listening to requests on http://localhost:${port}`);
+});
