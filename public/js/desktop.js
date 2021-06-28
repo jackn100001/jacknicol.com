1e136586d52aeb40cc8f26594ccc02f35822ab2f
Jack <jack-nicol1@hotmail.co.uk>
2021-06-28 19:07:05 +0100

    Fix date/time in the taskbar

--- public/js/desktop.js
+++ public/js/desktop.js
@@ -1,9 +1,11 @@
 $(document).ready(function() {
-	var dt = new Date();
-	var ampm = (dt.getHours() >= 12) ? "PM" : "AM";
-	var time = dt.getHours().toString().padStart(2, '0') + ":" + dt.getMinutes().toString().padStart(2, '0') + " " + ampm;
+	// var dt = new Date();
+	// var ampm = (dt.getHours() >= 12) ? "PM" : "AM";
+	// var time = dt.getHours().toString().padStart(2, '0') + ":" + dt.getMinutes().toString().padStart(2, '0') + " " + ampm;
 	
-	$(".datetime").text(time);
+	// $(".datetime").text(time);
+	startTime();
+
 
 	$(".icon").on("click", function() {
 		var windowName = $(this).data('window');
@@ -31,3 +33,26 @@ $(document).ready(function() {
 $(document).on("click", ".close-window", function() {
 	$(this).closest(".window").remove();
 });
+
+function startTime() {
+  	var today = new Date();
+
+	var dd = String(today.getDate()).padStart(2, '0');
+	var mm = String(today.getMonth() + 1).padStart(2, '0');
+	var yyyy = today.getFullYear();
+	var currentDate = dd + '/' + mm + '/' + yyyy;
+
+	var h = today.getHours();
+  	var m = today.getMinutes();
+  	m = checkTime(m);
+
+  	$(".datetime").text(currentDate + " " + h + ":" + m);
+
+  	var t = setTimeout(startTime, 1000);
+}
+
+
+function checkTime(i) {
+	if (i < 10) {i = "0" + i};
+ 	return i;
+}
